07a5d58338e27cbb6ea516ab17d181fbb7735971
abetomoki0420 <abe.tomoki@cloud-ace.jp>
2020-10-05 13:19:08 +0900

    feat: レイアウト調整 タスク追加処理を追加

--- src/components/Todo.vue
+++ src/components/Todo.vue
@@ -1,16 +1,84 @@
 <template>
   <div class="container">
     <h1>Todo App</h1>
+    <div class="input-form">
+      <label for="todo_name">タスク名</label>
+      <input
+        v-model="todoName"
+        id="todo_name"
+        class="input-form-name"
+        type="text"
+      />
+      <button @click="registerTodo" class="input-form-submit">登録</button>
+    </div>
+    <ul class="todos-container">
+      <li>
+        <div class="todos-container-item">
+          <span>
+            todo1
+          </span>
+          <span>
+            [X]
+          </span>
+        </div>
+      </li>
+      <li>
+        <div class="todos-container-item">
+          <span>
+            todo1
+          </span>
+          <span>
+            [X]
+          </span>
+        </div>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script lang="ts">
-export default {
-  name: "Todo"
-};
+import Vue from "vue";
+import todoService from "@/service/TodoService";
+
+interface Data {
+  todoName: string;
+}
+
+export default Vue.extend({
+  name: "Todo",
+  data: (): Data => ({
+    todoName: ""
+  }),
+  methods: {
+    registerTodo() {
+      todoService.create(this.todoName);
+    }
+  }
+});
 </script>
 
 <style lang="scss">
+// 全体のレイアウト
 .container {
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+}
+
+// タスク入力フォームのレイアウト
+.input-form {
+  &-name {
+    margin-left: 4px;
+  }
+  &-submit {
+    margin-left: 4px;
+  }
+}
+
+// Todoタスクのレイアウト
+.todos {
+  &-container {
+    background-color: white;
+  }
 }
 </style>

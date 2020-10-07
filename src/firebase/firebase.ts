f90222f1cd0789b9f02e3ded2e69096c68d49698
iganari <cigaguri@gmail.com>
2020-10-07 15:29:55 +0900

    firebase.ts のvalue を抜く

--- src/firebase/firebase.ts
+++ src/firebase/firebase.ts
@@ -1,14 +1,13 @@
 import firebase from "firebase";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDTIROlxUxY-N8KtkZJHyoakZtQlxatpRA",
-  authDomain: "abetomo-sandbox.firebaseapp.com",
-  databaseURL: "https://abetomo-sandbox.firebaseio.com",
-  projectId: "abetomo-sandbox",
-  storageBucket: "abetomo-sandbox.appspot.com",
-  messagingSenderId: "526273012962",
-  appId: "1:526273012962:web:570bf33a6b8bd81ab2eabb",
-  measurementId: "G-XLYR7V0MY6"
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
 };
 
 firebase.initializeApp(firebaseConfig);
